39b5a81f62ded285d60d77719ab0029b65c64812
Eren Karakas <erenkarakas.ek@gmail.com>
2022-04-07 19:14:14 +0300

    Special Middlewares

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const pageControllers = require('../controllers/pageControllers');
+const redirectMiddlewares = require('../middlewares/redirectMiddlewares');
 
 const router = express.Router();
 
 router.route('/').get(pageControllers.getIndexPage);
 router.route('/about').get(pageControllers.getAboutPage);
-router.route('/register').get(pageControllers.getRegisterPage);
-router.route('/login').get(pageControllers.getLoginPage);
+router.route('/register').get(redirectMiddlewares, pageControllers.getRegisterPage);
+router.route('/login').get(redirectMiddlewares, pageControllers.getLoginPage);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authControllers = require('../controllers/authControllers');
+const authMiddlewares = require('../middlewares/authMiddlewares');
 
 const router = express.Router();
 
 router.route('/signup').post(authControllers.createUser);
 router.route('/login').post(authControllers.loginUser);
 router.route('/logout').get(authControllers.logoutUser);
-router.route('/dashboard').get(authControllers.getDashboardPage);
+router.route('/dashboard').get(authMiddlewares, authControllers.getDashboardPage);
 
 module.exports = router;
